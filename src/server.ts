27bd3dba2370a45e2515e194c10b64430b6edd54
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-05-18 18:34:00 -0300

    create flux of authentication

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
-import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -8,7 +12,12 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
