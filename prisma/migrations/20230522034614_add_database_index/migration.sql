80d85b80e528eeb0abd490eaa89823e352b1495c
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-05-22 00:57:36 -0300

    test

--- prisma/migrations/20230522034614_add_database_index/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `Memory` DROP FOREIGN KEY `Memory_userId_fkey`;
-
--- RenameIndex
-ALTER TABLE `Memory` RENAME INDEX `Memory_userId_fkey` TO `Memory_userId_idx`;
--- prisma/migrations/20230522035719_create_mysql/migration.sql
+++ prisma/migrations/20230522035719_create_mysql/migration.sql
@@ -19,8 +19,6 @@ CREATE TABLE `Memory` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `userId` VARCHAR(191) NOT NULL,
 
+    INDEX `Memory_userId_idx`(`userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `Memory` ADD CONSTRAINT `Memory_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
