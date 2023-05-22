95f347c166fe5c62d602529a6f0e54c65fc85f0d
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-05-21 21:33:11 -0300

    change bd to postgres

--- src/server.ts
+++ src/server.ts
@@ -32,8 +32,9 @@ app.register(memoriesRoutes)
 
 app
   .listen({
-    port: 3333,
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
   })
   .then(() => {
-    console.log('🔥HTTP server running on http://localhost:3333')
+    console.log('🔥HTTP server running')
   })
