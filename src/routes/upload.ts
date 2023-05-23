e73f71a8361afd731d669915cd7cafa780dec559
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-05-23 00:57:10 -0300

    testeee

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,11 +1,6 @@
-import { extname } from 'node:path'
-import { Storage } from '@google-cloud/storage'
-import { randomUUID } from 'node:crypto'
-import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
-import { promisify } from 'node:util'
-import { PassThrough, pipeline, Readable } from 'node:stream'
+import { FastifyInstance } from 'fastify'
 
-const pipelineAsync = promisify(pipeline)
+import { Storage } from '@google-cloud/storage'
 
 const storage = new Storage({
   projectId: process.env.GCLOUD_STORAGE_PROJECT_ID,
@@ -15,49 +10,33 @@ const storage = new Storage({
   },
 })
 
-async function getBufferFromStream(stream: Readable): Promise<Buffer> {
-  const passThrough = new PassThrough()
-  const chunks: Buffer[] = []
-
-  pipeline(stream, passThrough, (err) => {
-    if (err) {
-      throw err
-    }
-  })
-
-  passThrough.on('data', (chunk) => {
-    chunks.push(chunk)
-  })
-
-  await pipelineAsync(stream, passThrough)
-
-  return Buffer.concat(chunks)
-}
+const bucketName = 'spacetime-bucket'
 
 export async function uploadRoutes(app: FastifyInstance) {
-  app.post('/upload', async (request: FastifyRequest, reply: FastifyReply) => {
-    const parts = request.parts() as any
+  app.post('/upload', async (request, reply) => {
+    const parts = request.parts()
+    let fileBuffer: any
 
     for await (const part of parts) {
       if (part.file) {
-        const fileBuffer = await getBufferFromStream(part.file)
+        const buffers = []
 
-        // Upload fileBuffer to Google Cloud Storage
-        const bucketName = process.env.GCLOUD_STORAGE_BUCKET as string
+        for await (const chunk of part.file) {
+          buffers.push(chunk)
+        }
+        fileBuffer = Buffer.concat(buffers)
 
-        const fileId = randomUUID()
-        const extension = extname(part.filename)
-
-        const destination = fileId.concat(extension)
-
-        const bucket = storage.bucket(bucketName)
-        const file = bucket.file(destination)
-        await file.save(fileBuffer)
-
-        const fileUrl = `https://storage.googleapis.com/${bucket.name}/${destination}`
-
-        reply.send({ success: true, fileUrl })
+        reply.send({ success: true, message: 'File uploaded successfully' })
       }
     }
+
+    const destination = 'asd.jpeg'
+
+    const bucket = storage.bucket(bucketName)
+    const blob = bucket.file(destination)
+
+    await blob.createWriteStream().end(fileBuffer)
+
+    reply.send({ success: true, message: 'File uploaded successfully' })
   })
 }
