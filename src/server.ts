96f5fdb23e1e58aac1b88e63df295d399ad3fb83
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-05-23 13:39:08 -0300

    test proxy

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,6 @@ import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
 import { uploadRoutes } from './routes/upload'
 import cors from '@fastify/cors'
-import fs from 'fs'
 
 // const app = fastify({
 //   http2: true,
@@ -28,11 +27,9 @@ app.register(jwt, {
   secret: 'spacetime',
 })
 
-app.register(require('fastify-https-redirect'))
-
 app.register(require('@fastify/http-proxy'), {
-  upstream: 'https://storage.googleapis.com',
-  prefix: '/api',
+  upstream: 'https://console.cloud.google.com',
+  prefix: '/storage',
   http2: false, // optional
 })
 
@@ -42,7 +39,7 @@ app.register(memoriesRoutes)
 
 app
   .listen({
-    host: process.env.HOST ? String(process.env.HOST) : '0.0.0.0',
+    host: '0.0.0.0',
     port: process.env.PORT ? Number(process.env.PORT) : 3333,
   })
   .then(() => {
