4faa37db03d3277dce7956fe346e01ad796cfc6d
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-05-22 21:53:04 -0300

    integrate with google cloud storage

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,84 +1,59 @@
-import { randomUUID } from 'node:crypto'
-import { FastifyInstance } from 'fastify'
-import { extname, resolve } from 'node:path'
-import { createWriteStream } from 'node:fs'
-import { promisify } from 'node:util'
-import { pipeline } from 'node:stream'
+import path, { extname } from 'node:path'
 import { Storage } from '@google-cloud/storage'
-import { request } from 'node:http'
-import { format } from 'util'
+import { randomUUID } from 'node:crypto'
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import { promisify } from 'util'
+import { PassThrough, pipeline, Readable } from 'stream'
 
-const pump = promisify(pipeline)
+const pipelineAsync = promisify(pipeline)
 
-const storage = new Storage({ keyFilename: 'google-cloud-key.json' })
-const bucket = storage.bucket('bezkoder-e-commerce')
+const storage = new Storage({
+  keyFilename: path.join(__dirname, '../../google-cloud-key.json'),
+})
 
 export async function uploadRoutes(app: FastifyInstance) {
-  app.post('/upload', async (request, reply) => {
-    const upload = await request.file({
-      limits: {
-        fileSize: 5_242_800, // 5mb
-      },
-    })
+  app.post('/upload', async (request: FastifyRequest, reply: FastifyReply) => {
+    const parts = request.parts()
 
-    if (!upload) {
-      return reply.status(400).send()
-    }
+    for await (const part of parts) {
+      if (part.file) {
+        const fileBuffer = await getBufferFromStream(part.file)
 
-    const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
-    const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
+        // Upload fileBuffer to Google Cloud Storage
+        const bucketName = 'spacetime-bucket'
 
-    if (!isValidFileFormat) {
-      return reply.status(400).send()
-    }
+        const fileId = randomUUID()
+        const extension = extname(part.filename)
 
-    const fileId = randomUUID()
-    const extension = extname(upload.filename)
+        const destination = fileId.concat(extension)
 
-    const fileName = fileId.concat(extension)
+        const bucket = storage.bucket(bucketName)
+        const file = bucket.file(destination)
+        await file.save(fileBuffer)
 
-    const blob = bucket.file(fileName)
-    const blobStream = blob.createWriteStream({
-      resumable: false,
-    })
+        const fileUrl = `https://storage.googleapis.com/${bucket.name}/${destination}`
 
-    blobStream.on('error', (err) => {
-      reply.status(500).send({ message: err.message })
-    })
-
-    blobStream.on('finish', async (data) => {
-      // Create URL for directly file access via HTTP.
-      const publicUrl = format(
-        `https://storage.googleapis.com/${bucket.name}/${blob.name}`,
-      )
-
-      try {
-        // Make the file public
-        await bucket.file(fileName).makePublic()
-      } catch {
-        return reply.status(500).send({
-          message: `Uploaded the file successfully: ${fileName}, but public access is denied!`,
-          url: publicUrl,
-        })
+        reply.send({ success: true, fileUrl })
       }
-
-      reply.status(200).send({
-        message: 'Uploaded the file successfully: ' + fileName,
-        url: publicUrl,
-      })
-    })
-
-    blobStream.end(request.file.buffer)
-
-    /* const writeStream = createWriteStream(
-      resolve(__dirname, '../../uploads', fileName),
-    )
-
-    await pump(upload.file, writeStream)
-
-    const fullUrl = request.protocol.concat('://').concat(request.hostname)
-    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
-    return { fileUrl }
-    */
+    }
   })
+}
+
+async function getBufferFromStream(stream: Readable): Promise<Buffer> {
+  const passThrough = new PassThrough()
+  const chunks: Buffer[] = []
+
+  pipeline(stream, passThrough, (err) => {
+    if (err) {
+      throw err
+    }
+  })
+
+  passThrough.on('data', (chunk) => {
+    chunks.push(chunk)
+  })
+
+  await pipelineAsync(stream, passThrough)
+
+  return Buffer.concat(chunks)
 }
--- src/server.ts
+++ src/server.ts
@@ -6,17 +6,11 @@ import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
 import { uploadRoutes } from './routes/upload'
-import { resolve } from 'node:path'
 import cors from '@fastify/cors'
 
 const app = fastify()
 
 app.register(multipart)
-
-app.register(require('@fastify/static'), {
-  root: resolve(__dirname, '../uploads'),
-  prefix: '/uploads',
-})
 
 app.register(cors, {
   origin: true,
@@ -26,8 +20,8 @@ app.register(jwt, {
   secret: 'spacetime',
 })
 
-app.register(authRoutes)
 app.register(uploadRoutes)
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
