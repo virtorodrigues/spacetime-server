5b9ad4f83e8c4dd1f55606025b8c4f5782baca05
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-05-23 01:12:46 -0300

    testeeeaasddfg

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 
 import { Storage } from '@google-cloud/storage'
+import { randomUUID } from 'crypto'
+import { extname } from 'path'
 
 const storage = new Storage({
   projectId: process.env.GCLOUD_STORAGE_PROJECT_ID,
@@ -14,29 +16,35 @@ const bucketName = 'spacetime-bucket'
 
 export async function uploadRoutes(app: FastifyInstance) {
   app.post('/upload', async (request, reply) => {
-    const parts = request.parts()
+    const parts: any = request.parts()
+
     let fileBuffer: any
+
+    let filename = ''
 
     for await (const part of parts) {
       if (part.file) {
         const buffers = []
 
+        const fileId = randomUUID()
+        const extension = extname(part.filename)
+
+        filename = fileId.concat(extension)
+
         for await (const chunk of part.file) {
           buffers.push(chunk)
         }
         fileBuffer = Buffer.concat(buffers)
-
-        reply.send({ success: true, message: 'File uploaded successfully' })
       }
     }
 
-    const destination = 'asd.jpeg'
-
     const bucket = storage.bucket(bucketName)
-    const blob = bucket.file(destination)
+    const blob = bucket.file(filename)
 
     await blob.createWriteStream().end(fileBuffer)
 
-    reply.send({ success: true, message: 'File uploaded successfully' })
+    const fileUrl = `https://storage.googleapis.com/${bucket.name}/${filename}`
+
+    reply.send({ success: true, fileUrl })
   })
 }
